ab36ff79bea4e4727c502551db686b9365e058c2
Evgeny1337 <genya-sozykin@mal.ru>
2025-06-21 22:18:05 +0500

    moved the token receiving to the main function

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def create_message(name, url, is_negative=False):
     return f"У вас проверили работу '{name}'\n\nтПреподавателю все понравилось можете преступать к следующему уроку!\n\n[{name}]({url})\."
 
 
-def check_reviews(bot:telegram.Bot, chat_id):
-    devman_token = environ['DEVMAN_TOKEN']
+def check_reviews(bot:telegram.Bot, chat_id, devman_token):
     headers = {'Authorization':devman_token}
     params = {}
     while True:
@@ -38,16 +37,19 @@ def check_reviews(bot:telegram.Bot, chat_id):
 
 
 def start_handler(update, context):
+    devman_token = context.bot_data.get('devman_token')
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="В данный чат с id {} будцт приходить уведомления о проверке работ".format(chat_id))
-    check_reviews(context.bot, chat_id)
+    context.bot.send_message(chat_id=chat_id, text="В данный чат с id {} будут приходить уведомления о проверке работ".format(chat_id))
+    check_reviews(context.bot, chat_id, devman_token)
     
 
-def start_bot():
+def main():
     load_dotenv()
+    devman_token = environ['DEVMAN_TOKEN']
     tg_token = environ['TG_TOKEN']
     updater = telegram.ext.Updater(token=tg_token, use_context=True)
     dispatcher = updater.dispatcher
+    dispatcher.bot_data['devman_token'] = devman_token
     start_h = telegram.ext.CommandHandler("start",start_handler)
     dispatcher.add_handler(start_h)
     updater.start_polling()
@@ -55,4 +57,4 @@ def start_bot():
 
 
 if __name__ == '__main__':
-    start_bot()
+    main()
